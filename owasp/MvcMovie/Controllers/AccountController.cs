fe3090dc7878ec555b7bc7ed4c22977c583e6ca0
Phil <philliptjohnson@hotmail.com>
2021-09-10 20:26:42 +0100

    Added Backoff and Cutoff

--- owasp/MvcMovie/Controllers/AccountController.cs
+++ owasp/MvcMovie/Controllers/AccountController.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,28 +18,78 @@ namespace LearnASPNETCoreMVC5.Controllers
             return View();
         }
         
-        [Route("login")]
-        [HttpPost]
-        public IActionResult Login(string username, string password)
-        {
-            if (username != null && password != null && username.Equals("phil") && password.Equals("Password1!"))
-            {
-                HttpContext.Session.SetString("username", username);
-                return View("Success");
-            }
-            else
-            {
-                ViewBag.error = "Invalid Account";
-                return View("Index");
-            }
-        }
-
         [Route("logout")]
         [HttpGet]
         public IActionResult Logout()
         {
             HttpContext.Session.Remove("username");
             return RedirectToAction("Index");
+        }
+        
+        [Route("login")]
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            /*
+            if (Cutoff())
+            {
+                return View("TooManyInvalidLoginAttempts");
+            }
+            */
+            
+            if (username != null && password != null && username.Equals("phil") && password.Equals("Password1!"))
+            {
+                //ClearBackoff();
+
+                HttpContext.Session.SetString("username", username);
+                return View("Success");
+            }
+            else
+            {
+                //Backoff();
+
+                ViewBag.error = "Invalid Account";
+                return View("Index");
+            }
+        }
+
+        private void ClearBackoff()
+        {
+            HttpContext.Session.SetString("backoff", "0");
+        }
+
+        private void Backoff()
+        {
+            int backoff = 25;
+            if (HttpContext.Session.GetInt32("backoff") != null)
+            {
+                backoff = (int) HttpContext.Session.GetInt32("backoff");
+                Console.WriteLine($"backoff={backoff}");
+            }
+
+            bool delayed = Delay(backoff).Result;
+
+            HttpContext.Session.SetInt32("backoff", backoff * 2);
+        }
+
+        async Task<bool> Delay(int milliseconds)
+        {
+            await Task.Delay(milliseconds);
+            return true;
+        }
+        
+        private bool Cutoff()
+        {
+            if (HttpContext.Session.GetInt32("backoff") != null)
+            {
+                int backoff = (int) HttpContext.Session.GetInt32("backoff");
+                if (backoff > 500)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
